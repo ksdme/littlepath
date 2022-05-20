9177c7086da9a201798b3ae1d15ee49b00d7dd35
Kilari Teja <kilariteja9@gmail.com>
2022-05-21 02:19:38 +0530

    Use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,31 @@ mod littlepath;
 
 use std::env;
 use std::path::PathBuf;
+use clap::Parser;
 use path_absolutize::*;
 
+/// Use little paths to address your files and directories.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Base directory for the search.
+    #[clap(short, long, default_value = ".")]
+    base_directory: String,
+
+    /// littlepath to expand.
+    query: String,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
     // The littlepath in question to be expanded.
-    let query_option = args.get(1).unwrap();
-    let query = PathBuf::from(query_option);
+    let query = PathBuf::from(args.query);
 
     // The directory relative to which the little path expansion should happen,
     // defaults to the working directory of the shell this program is invoked
     // from.
-    let default_relative_to = ".".to_owned();
-    let relative_to_option = args.get(2)
-        .unwrap_or(&default_relative_to);
-    let relative_to = PathBuf::from(relative_to_option)
+    let relative_to = PathBuf::from(args.base_directory)
         .absolutize_from(&env::current_dir().unwrap())
         .unwrap()
         .to_path_buf();
